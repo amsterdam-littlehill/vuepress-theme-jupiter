082c66a532d28a64e21be9d2831fd9d20cd761d2
Zilong Liang <zlliang15@fudan.edu.cn>
2019-01-23 10:32:07 +0800

    Add nprogress support

--- layouts/Layout.vue
+++ layouts/Layout.vue
@@ -8,7 +8,9 @@
 </template>
 
 <script>
+import Vue from 'vue'
 import dayjs from 'dayjs'
+import nprogress from 'nprogress'
 
 import Header from './partial/Header.vue'
 import Footer from './partial/Footer.vue'
@@ -28,12 +30,24 @@ export default {
   },
   mounted: function() {
     document.addEventListener("touchstart", function(){}, true)
+    // nprogress
+    nprogress.configure({ showSpinner: false })
+    this.$router.beforeEach((to, from, next) => {
+      if (to.path !== from.path && !Vue.component(to.name)) {
+        nprogress.start()
+      }
+      next()
+    })
+    this.$router.afterEach(function() {
+      nprogress.done()
+    })
   }
 }
 </script>
 
 <style src="@fortawesome/fontawesome-free/css/all.min.css"></style>
 <style src="bulma-jupiter/dist/bulma-jupiter.min.css"></style>
+<style src="../styles/nprogress.css"></style>
 <style src="../styles/fix.css"></style>
 
 <style>
